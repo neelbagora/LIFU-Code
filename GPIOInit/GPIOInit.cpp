9c22333b8620eada22388db9e381be436565d1eb
neelbagora <neelbagora@gmail.com>
2018-10-13 14:58:46 -0700

    UPDATE comments

--- GPIOInit/GPIOInit.cpp
+++ GPIOInit/GPIOInit.cpp
@@ -12,31 +12,31 @@ void GPIOInit::MX_GPIO_Init(void)
     //Master Timer Output
     GPIO_InitTypeDef GPIO;
     GPIO.Pin       = GPIO_PIN_9;                                                //PA9
-    GPIO.Mode      = GPIO_MODE_AF_PP;
-    GPIO.Speed     = GPIO_SPEED_FREQ_VERY_HIGH;
-    GPIO.Pull      = GPIO_NOPULL;
-    GPIO.Alternate = GPIO_AF1_TIM1;
-    HAL_GPIO_Init(GPIOA, &GPIO); 
+    GPIO.Mode      = GPIO_MODE_AF_PP;                                           //Mode- Push Pull
+    GPIO.Speed     = GPIO_SPEED_FREQ_VERY_HIGH;                                 //Frequency-Very High
+    GPIO.Pull      = GPIO_NOPULL;                                               //DeToggle pull
+    GPIO.Alternate = GPIO_AF1_TIM1;                                             //AlternateFunction3- Timer 1
+    HAL_GPIO_Init(GPIOA, &GPIO);                                                //Init
     
-    //Positive PWM Output                                                       //PC6
-    GPIO.Pin                     = GPIO_PIN_6;
-    GPIO.Mode                    = GPIO_MODE_AF_PP;
-    GPIO.Speed                   = GPIO_SPEED_FREQ_VERY_HIGH;
-    GPIO.Pull                    = GPIO_NOPULL;
-    GPIO.Alternate               = GPIO_AF3_TIM8;
-    HAL_GPIO_Init(GPIOC, &GPIO);
+    //Positive PWM Output                                                       
+    GPIO.Pin                     = GPIO_PIN_6;                                  //PC6
+    GPIO.Mode                    = GPIO_MODE_AF_PP;                             //Mode- Push Pull
+    GPIO.Speed                   = GPIO_SPEED_FREQ_VERY_HIGH;                   //Frequency-Very High
+    GPIO.Pull                    = GPIO_NOPULL;                                 //DeToggle pull
+    GPIO.Alternate               = GPIO_AF3_TIM8;                               //AlternateFunction3- Timer 8
+    HAL_GPIO_Init(GPIOC, &GPIO);                                                //Init
     
-    //NEGATIVE GPIO CONFIG CAUSES PROGRAM HANG, NEEDS TO BE FIXED
+    //NEGATIVE GPIO CONFIG FIXED
     GPIO.Pin       = GPIO_PIN_7;                                                //PA7
-    GPIO.Mode      = GPIO_MODE_AF_PP;
-    GPIO.Speed     = GPIO_SPEED_FREQ_VERY_HIGH;
-    GPIO.Pull      = GPIO_NOPULL;
-    GPIO.Alternate = GPIO_AF3_TIM8;
-    HAL_GPIO_Init(GPIOA, &GPIO);
+    GPIO.Mode      = GPIO_MODE_AF_PP;                                           //Mode- Push Pull
+    GPIO.Speed     = GPIO_SPEED_FREQ_VERY_HIGH;                                 //Frequency-Very High
+    GPIO.Pull      = GPIO_NOPULL;                                               //DeToggle pull
+    GPIO.Alternate = GPIO_AF3_TIM8;                                             //AlternateFunction3- Timer 8
+    HAL_GPIO_Init(GPIOA, &GPIO);                                                //Init
 }
 
-void GPIOInit::MX_GPIO_DeInit() {
-    HAL_GPIO_DeInit(GPIOC, GPIO_PIN_6);
-    HAL_GPIO_DeInit(GPIOA, GPIO_PIN_7);
-    HAL_GPIO_DeInit(GPIOA, GPIO_PIN_9);
+void GPIOInit::MX_GPIO_DeInit() {                                               //DeInit
+    HAL_GPIO_DeInit(GPIOC, GPIO_PIN_6);                                         //PC6
+    HAL_GPIO_DeInit(GPIOA, GPIO_PIN_7);                                         //PC7
+    HAL_GPIO_DeInit(GPIOA, GPIO_PIN_9);                                         //PC9
 }
